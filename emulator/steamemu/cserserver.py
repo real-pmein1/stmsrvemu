c232ee6067954cda6d8c15ee9a02e3e6548a544c
cystface-man <shefben@gmail.com>
2023-06-01 18:12:30 -0500

    fixed sendto in cser, added correct reply for survey.
    please note: although the reply is technically correct, the results page is missing results.  but you no longer get an error, so... high five?

--- emulator/steamemu/cserserver.py
+++ emulator/steamemu/cserserver.py
@@ -1,21 +1,23 @@
-import threading, logging, struct, binascii, time, socket, ipaddress, os.path, ast
+import threading, logging, struct, binascii, time, socket, ipaddress, os.path, ast, csv
 import os
 import steam
 import config
 import steamemu.logger
 import globalvars
-
+from Crypto.Cipher import AES
 from steamemu.config import read_config
 
 config = read_config()
 
 class cserserver(threading.Thread):
+    serversocket = None
 
     def __init__(self, host, port):
         #threading.Thread.__init__(self)
         self.host = host
         self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        serversocket = self.socket
 
     def start(self):
         
@@ -101,15 +103,98 @@ class cserserver(threading.Thread):
             except Exception as e:
                 log.debug(clientid + "Failed to receive client crash report: " + str(e))
         elif data.startswith("q"):  # 71
-            log.info("Received encrypted ICE client stats - INOP")
+            CSER_ICE_KEY_q = bytearray([0x36, 0xAF, 0xA5, 0x05, 0x4C, 0xFB, 0x1D, 0x71])
+            CSER_ICE_KEY_q = CSER_ICE_KEY_q.ljust(16, b'\x00')  # Pad with zeros to make it 16 bytes
+            aes_q = AES.new(str(CSER_ICE_KEY_q), AES.MODE_ECB)
+            log.info("Received encrypted ICE client stats")
+                # C2M_BUGREPORT details
+                #     u8(C2M_UPLOADDATA_PROTOCOL_VERSION)
+                #     u16(encryptedlength)
+                #     remainder=encrypteddata
+
+                # encrypted payload:
+                #     byte(corruptionid)
+                #     byte(protocolid) // C2M_UPLOADDATA_DATA_VERSION
+                #     string(tablename 40)
+                #     u8(numvalues)
+                #     for each value:
+                #         string(fieldname 32)
+                #         string(value 128)
+            """len = struct.unpack('H', data[1:3])[0]
+                sentData = data[3:]
+
+                log.debug("Received crypted Steam client stats packet, from %s: %s" % (from_address, sentData))
+
+                decrypted = ''
+
+                # Create the AES decryption object
+                aes = AES.new(CSER_ICE_KEY_q, AES.MODE_ECB)
+
+                for ind in range(len / 8):
+                    encrypted_block = sentData[ind * 8:(ind + 1) * 8]
+                    decrypted_block = aes.decrypt(encrypted_block)
+                    decrypted += decrypted_block
+
+                log.debug("Received Steam client stats packet, from %s: %s" % (from_address, decrypted))
+
+                debug = ''
+
+                header = struct.unpack('H', decrypted[:2])[0]
+                if header != 0x0101:
+                    log.debug("Invalid stats header, from %s: %s" % (from_address, decrypted))
+                    return 0
+
+                subject = decrypted[2:]
+
+                keys = []
+                values = []
+
+                debug += "\r\n%s : \r\n" % subject
+                next_index = 2 + len(subject) + 1
+                nbFields = ord(decrypted[next_index])
+                next_index += 1
+
+                for ind in range(nbFields):
+                    tmp = ""
+                    key = decrypted[next_index:].split('\x00', 1)[0]
+                    next_index += len(key) + 1
+
+                    value = decrypted[next_index:].split('\x00', 1)[0]
+                    next_index += len(value) + 1
+
+                    keys.append(key)
+                    values.append(value)
+                    tmp = "  %s: %s\r\n" % (key, value)
+                    debug += tmp
+
+                log.debug("Received Steam client stats, from %s: %s" % (from_address, debug))
+
+                # Save key-value pairs to a CSV file
+                filename = os.path.join('./logs', from_address[0] + '.bugreport#' + decrypted[0] + '.csv')
+                with open(filename, 'w') as csvfile:
+                    writer = csv.writer(csvfile)
+                    for key, value in zip(keys, values):
+                        writer.writerow([key, value])
+
+                # Clean up
+                del aes
+                del decrypted
+                del debug"""
+        
         elif data.startswith("a"):  # 61
+            log.info("Received app download stats - INOP")
+        elif data.startswith("o"):  # 61
             log.info("Received app download stats - INOP")
         elif data.startswith("i"):  # 69
             log.info("Received unknown stats - INOP")
         elif data.startswith("k"):  # 6b
             log.info("Received app usage stats - INOP")
         elif data.startswith("g"):  # survey response
+            CSER_ICE_KEY_g = bytearray([0x1B, 0xC8, 0x0D, 0x0E, 0x53, 0x2D, 0xB8, 0x36])
+            CSER_ICE_KEY_g = CSER_ICE_KEY_g.ljust(16, b'\x00')  # Pad with zeros to make it 16 bytes
+            aes_g = AES.new(str(CSER_ICE_KEY_g), AES.MODE_ECB)
             log.info("Received Survey Response (no actions taken)")
+            self.socket.sendto("\xFF\xFF\xFF\xFF\x68\x01",address)
         else:
             log.info("Unknown CSER command: %s" % data)
 
